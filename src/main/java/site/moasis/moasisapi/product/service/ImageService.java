a190067d1c96225fc66d663af6bb146785619a19
Jerry-hakjae <jerry@lightscale.io>
2024-06-16 20:28:27 +0900

    fix: add connect image service

---
+++ src/main/java/site/moasis/moasisapi/product/service/ImageService.java
@@ -0,0 +1,50 @@
+package site.moasis.moasisapi.product.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import site.moasis.moasisapi.common.exception.BadRequestException;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    @Qualifier("imageWebClient")
+    private final WebClient imageWebClient;
+
+    @Value("${app.image-server-url}")
+    private String imageScriptUrl;
+
+    public String uploadFile(String encodedFileBase64) {
+        return imageWebClient.post()
+                .uri(imageScriptUrl)
+                .body(BodyInserters.fromFormData("myFile", encodedFileBase64)
+                        .with("mimeType", "image/png")
+                        .with("fileName", "product.png"))
+                .exchangeToMono(this::handleRedirect)
+                .block();
+    }
+
+    private Mono<String> handleRedirect(ClientResponse response) {
+        if (response.statusCode().is3xxRedirection()) {
+            String location = Objects.requireNonNull(response.headers().asHttpHeaders().getLocation()).toString();
+            return imageWebClient.get()
+                    .uri(location)
+                    .retrieve()
+                    .bodyToMono(String.class);
+        } else {
+            return response.bodyToMono(String.class);
+        }
+    }
+}
