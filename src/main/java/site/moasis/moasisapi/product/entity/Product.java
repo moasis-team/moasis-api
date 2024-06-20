901e4decb59534663556eddd35accdba5b6f6896
Jerry-hakjae <jerry@lightscale.io>
2024-06-21 00:38:26 +0900

    fix: add baseentity

--- src/main/java/site/moasis/moasisapi/product/entity/Product.java
+++ src/main/java/site/moasis/moasisapi/product/entity/Product.java
@@ -1,27 +1,17 @@
 package site.moasis.moasisapi.product.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
+public class Product extends BaseEntity {
 
     private String name;
     private Long price;
@@ -31,11 +21,4 @@ public class Product {
     private int quantity;
     private String productCode;
     private String productNumber;
-
-    // createdAt
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
-
 }
