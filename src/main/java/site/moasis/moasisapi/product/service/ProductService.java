da4d45d61511fd548aff57540c46c727b04b8845
Jchan <gkswocks123@gmail.com>
2024-06-25 21:44:46 +0900

    refac: remove try-catch code and add @Transactional

--- src/main/java/site/moasis/moasisapi/product/service/ProductService.java
+++ src/main/java/site/moasis/moasisapi/product/service/ProductService.java
@@ -5,18 +5,17 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import site.moasis.moasisapi.common.exception.BadRequestException;
 import site.moasis.moasisapi.common.exception.NotFoundException;
 import site.moasis.moasisapi.common.service.ImageService;
-import site.moasis.moasisapi.product.dto.ProductDto;
-import site.moasis.moasisapi.product.dto.ProductListDto;
-import site.moasis.moasisapi.product.dto.ReqCreateProductDto;
-import site.moasis.moasisapi.product.dto.ResCreateProductDto;
+import site.moasis.moasisapi.product.dto.CreateProductRequestDTO;
+import site.moasis.moasisapi.product.dto.CreateProductResponseDTO;
+import site.moasis.moasisapi.product.dto.GetProductResponseDTO;
 import site.moasis.moasisapi.product.entity.Product;
 import site.moasis.moasisapi.product.repository.ProductRepository;
 
 import java.util.Base64;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -25,76 +24,45 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final ImageService imageService;
 
-    public ResCreateProductDto createProduct(ReqCreateProductDto reqCreateProductDto) throws BadRequestException {
+    @Transactional()
+    public CreateProductResponseDTO createProduct(CreateProductRequestDTO createProductRequestDTO) throws BadRequestException {
         String productCode = UUID.randomUUID().toString();
-        String base64EncodedFile = Base64.getEncoder().encodeToString(reqCreateProductDto.getEncodedFile());
+        String base64EncodedFile = Base64.getEncoder().encodeToString(createProductRequestDTO.getEncodedFile());
         String imageUrl = imageService.uploadFile(base64EncodedFile);
-        try {
-            Product product = Product.builder()
-                .name(reqCreateProductDto.getName())
-                .price(reqCreateProductDto.getPrice())
-                .category(reqCreateProductDto.getCategory())
-                .imageUrl(imageUrl)
-                .details(reqCreateProductDto.getDetails())
-                .quantity(reqCreateProductDto.getQuantity())
-                .productCode(productCode)
-                .productNumber(reqCreateProductDto.getProductNumber())
-                .build();
-            productRepository.save(product);
-            return new ResCreateProductDto(productCode);
-        } catch (Exception e) {
-            throw new BadRequestException("상품 등록에 실패했습니다.");
-        }
+
+        Product product = Product.builder()
+            .name(createProductRequestDTO.getName())
+            .price(createProductRequestDTO.getPrice())
+            .category(createProductRequestDTO.getCategory())
+            .imageUrl(imageUrl)
+            .details(createProductRequestDTO.getDetails())
+            .quantity(createProductRequestDTO.getQuantity())
+            .productCode(productCode)
+            .productNumber(createProductRequestDTO.getProductNumber())
+            .build();
+        productRepository.save(product);
+        return new CreateProductResponseDTO(productCode);
     }
 
-    public ProductDto getProduct(String productCode) throws NotFoundException {
-        try {
-            Product product = productRepository.findByProductCode(productCode);
-            return ProductDto.builder()
-                .name(product.getName())
-                .price(product.getPrice())
-                .category(product.getCategory())
-                .imageUrl(product.getImageUrl())
-                .details(product.getDetails())
-                .quantity(product.getQuantity())
-                .productCode(product.getProductCode())
-                .productNumber(product.getProductNumber())
-                .build();
-        } catch (Exception e) {
-            throw new NotFoundException("상품을 찾을 수 없습니다.");
-        }
+    @Transactional()
+    public GetProductResponseDTO getProduct(String productCode) throws NotFoundException {
+        Product product = productRepository.findByProductCode(productCode);
+        return GetProductResponseDTO.builder()
+            .name(product.getName())
+            .price(product.getPrice())
+            .category(product.getCategory())
+            .imageUrl(product.getImageUrl())
+            .details(product.getDetails())
+            .quantity(product.getQuantity())
+            .productCode(product.getProductCode())
+            .productNumber(product.getProductNumber())
+            .build();
     }
 
-    public ProductListDto getProductList(String query, int offset, int pageSize) {
-        try {
-            Pageable pageable = PageRequest.of(offset, pageSize);
-            Slice<Product> productSlice = productRepository.findAllByNameContaining(query, pageable);
-
-            List<ProductDto> productDtoList = productSlice.getContent().stream().map(product -> ProductDto.builder()
-                .name(product.getName())
-                .price(product.getPrice())
-                .category(product.getCategory())
-                .imageUrl(product.getImageUrl())
-                .details(product.getDetails())
-                .quantity(product.getQuantity())
-                .productCode(product.getProductCode())
-                .productNumber(product.getProductNumber())
-                .build()
-            ).toList();
-
-            return ProductListDto.builder()
-                .productList(productDtoList)
-                .sliceMetaData(
-                    ProductListDto.SliceMetaData.builder()
-                        .totalProductQuantity(productSlice.getNumberOfElements())
-                        .hasNext(productSlice.hasNext())
-                        .build()
-                )
-                .build();
-
-        } catch (Exception e) {
-            throw new NotFoundException("상품을 찾을 수 없습니다.");
-        }
+    @Transactional()
+    public Slice<Product> getProductList(String query, int pageNumber, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        return productRepository.findAllByNameContaining(query, pageable);
     }
 }
 
