ec6b378e9445a4866525c66dab24864480bcc7b1
Jchan <gkswocks123@gmail.com>
2024-06-28 10:21:16 +0900

    feat: add validation in product http methods

--- src/main/java/site/moasis/moasisapi/product/dto/CreateProductRequestDTO.java
+++ src/main/java/site/moasis/moasisapi/product/dto/CreateProductRequestDTO.java
@@ -1,5 +1,7 @@
 package site.moasis.moasisapi.product.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,11 +10,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class CreateProductRequestDTO {
+    @NotBlank
     private String name;
+    @Min(0)
     private int price;
+    @NotBlank
     private String details;
+    @NotBlank
     private byte[] encodedFile;
+    @NotBlank
     private String category;
+    @Min(0)
     private int quantity;
+    @NotBlank
     private String productNumber;
 }
