e0c02b48ec694f54d7da21e71c02c1d321ebb721
Jerry-hakjae <jerry@lightscale.io>
2024-06-29 18:33:04 +0900

    fix: add common interceptor and error code
    fix: fix typo

--- src/main/java/site/moasis/moasisapi/common/exception/GlobalExceptionHandler.java
+++ src/main/java/site/moasis/moasisapi/common/exception/GlobalExceptionHandler.java
@@ -1,36 +1,46 @@
 package site.moasis.moasisapi.common.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.core.NestedExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import site.moasis.moasisapi.common.interceptor.CommonHttpRequestInterceptor;
 import site.moasis.moasisapi.common.response.CommonResponse;
+import site.moasis.moasisapi.common.response.ErrorCode;
 
-import java.util.Objects;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ServerException.class)
     public ResponseEntity<CommonResponse<Void>> handleCustomException(ServerException e) {
-        logger.info("[Error]: status: {}, message: {}", e.getStatus(), e.getMessage());
+        log.info("[Error]: status: {}, message: {}", e.getStatus(), e.getMessage());
         return CommonResponse.error(null, e.getStatus(), e.getMessage());
     }
 
-    // http 메서드 DTO validation 예외 처리
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<CommonResponse<Void>> handleConstraintViolationException(MethodArgumentNotValidException e) {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public CommonResponse methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
+        log.warn("[ServerException] eventId = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e).getMessage());
         BindingResult bindingResult = e.getBindingResult();
-        String errorMessage = Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage();
-
-        logger.info("[Validation Error]: {}", errorMessage);
-        return CommonResponse.error(null, HttpStatus.BAD_REQUEST, errorMessage);
+        FieldError fe = bindingResult.getFieldError();
+        if (fe != null) {
+            String message = "Request Error" + " " + fe.getField() + "=" + fe.getRejectedValue() + " (" + fe.getDefaultMessage() + ")";
+            return CommonResponse.error(ErrorCode.COMMON_INVALID_PARAMETER.name(), message);
+        } else {
+            return CommonResponse.error(ErrorCode.COMMON_INVALID_PARAMETER.name(), ErrorCode.COMMON_INVALID_PARAMETER.getErrorMsg());
+        }
     }
 }
 
