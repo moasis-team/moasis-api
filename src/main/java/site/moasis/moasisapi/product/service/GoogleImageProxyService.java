03d0153eccd8828c3e4e73f33f87ba910cbbf160
Sungwon Choi <csw2757@naver.com>
2024-06-30 12:46:53 +0900

    feat: image proxy api for google drive

---
+++ src/main/java/site/moasis/moasisapi/product/service/GoogleImageProxyService.java
@@ -0,0 +1,23 @@
+package site.moasis.moasisapi.product.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import site.moasis.moasisapi.client.ImageProxy;
+
+
+@Service
+@RequiredArgsConstructor
+public class GoogleImageProxyService implements ImageProxy {
+
+    private final WebClient googleImageProxy;
+
+    @Override
+    public Mono<byte[]> getImage(String imageId) {
+        return googleImageProxy.get()
+            .uri("/download?id=" + imageId)
+            .retrieve()
+            .bodyToMono(byte[].class);
+    }
+}
