5fd6292e81d1409faf3fa37150e43ec0aee1f104
Jerry-hakjae <jerry@lightscale.io>
2024-07-12 23:34:21 +0900

    fix: apply default local profile

--- src/main/java/site/moasis/moasisapi/MoasisApiApplication.java
+++ src/main/java/site/moasis/moasisapi/MoasisApiApplication.java
@@ -3,9 +3,16 @@ package site.moasis.moasisapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class MoasisApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MoasisApiApplication.class, args);
+        SpringApplication app = new SpringApplication(MoasisApiApplication.class);
+        Map<String, Object> defaultProperties = new HashMap<>();
+        defaultProperties.put("spring.profiles.default", "local");
+        app.setDefaultProperties(defaultProperties);
+        app.run(args);
     }
 }
