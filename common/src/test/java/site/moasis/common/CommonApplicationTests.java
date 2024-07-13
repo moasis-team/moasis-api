5156676c29f2114c825ea573f50961c1397d9b6b
Jerry-hakjae <jerry@lightscale.io>
2024-07-13 11:56:01 +0900

    feat: add ci and sonar qube
    fix: add gradle
    
    fix: add test yaml
    
    fix: build bugs

--- common/src/test/java/site/moasis/common/CommonApplicationTests.java
+++ common/src/test/java/site/moasis/common/CommonApplicationTests.java
@@ -1,9 +1,19 @@
 package site.moasis.common;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootTest
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaAuditing
+@Configuration
+@EnableJpaRepositories(basePackages = {"site.moasis.common.repository"})
+@EntityScan(basePackages = {"site.moasis.common.entity"})
 class CommonApplicationTests {
 
     @Test
--- image-processor/src/test/java/site/moasis/imageprocessor/ImageProcessorApplicationTests.java
+++ image-processor/src/test/java/site/moasis/imageprocessor/ImageProcessorApplicationTests.java
@@ -2,8 +2,10 @@ package site.moasis.imageprocessor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ImageProcessorApplicationTests {
 
     @Test
